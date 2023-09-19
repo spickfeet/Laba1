9dfa5e219e38fae2cb642d1078609dec7ba8f1a9
spickfeet <132142561+spickfeet@users.noreply.github.com>
2023-09-19 21:37:10 +1100

    Add files via upload

---
+++ AZZ-LB/LB-1/FoodProduct.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB_1
+{
+    internal class FoodProduct
+    {
+
+        private string _name;
+        public string Name { set { _name = value; } get { return _name; } }
+
+        private double _weight;
+
+        public double Weight { get { return _weight; } set { _weight = value; } }
+
+        private double _temperature;
+        public double Temperature { get { return _temperature; } set { _temperature = value; } }
+
+        private double _maxTemperature;
+        public double MaxTemperature { get { return _maxTemperature; } set { _maxTemperature = value; } }
+
+        private double _minTemperature;
+        public double MinTemperature { get { return _minTemperature; } set { _minTemperature = value; } }
+
+        private string _status = "Нормально";
+        public string Status { get { return _status; } set { _status = value; } }
+
+        private double _heatCapacity;
+        public double HeatCapacity { get { return _heatCapacity; } set { _heatCapacity = value; } }
+
+        public FoodProduct(string name = "", double weight = 0, double temperature = 0, double maxTemperature = 0, double minTemperature = 0, double heatCapacity = 0)
+        {
+            if (name == "") throw new ArgumentNullException("Неправильно указано имя продукта");    
+            if (weight <= 0) throw new ArgumentOutOfRangeException("Неправильно указана масса продукта");
+            if ((temperature <= -50) || (temperature > 70)) throw new ArgumentOutOfRangeException("Неправильно указана температура продукта");
+            if (heatCapacity == 0) throw new ArgumentOutOfRangeException("Неправильно указана теплоемкость продукта");
+            _name = name;
+            _weight = weight;
+            _temperature = temperature;
+            _maxTemperature = maxTemperature;
+            _minTemperature = minTemperature;
+            _heatCapacity = heatCapacity;
+            ChangeStatus();
+        }
+        private void ChangeStatus()
+        {
+            if (_temperature >= _maxTemperature) { _status = "Перегрет"; }
+            else { _status = "Переморожен"; }
+        }
+
+
+    }
+}
---
+++ AZZ-LB/LB-1/MainClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB_1
+{
+    class MainClass
+    {
+        static void Main()
+        {
+			try
+			{
+				FoodProduct foodProduct1 = new FoodProduct("Food", -1350, 39.6, 30, 20, 20);
+				Console.WriteLine(foodProduct1.Name);
+			}
+			catch (ArgumentNullException error)
+			{
+				Console.WriteLine(error.Message);
+			}
+
+			catch (ArgumentOutOfRangeException error)
+			{
+				Console.WriteLine(error.Message);
+			}
+
+			try {
+				FoodProduct foodProduct2 = new FoodProduct("Food", 1350, 139.6, 30, 20, 20);
+                Console.WriteLine(foodProduct2.Name);
+            }
+			catch (ArgumentNullException error) {
+                Console.WriteLine(error.Message);
+            }
+			catch (ArgumentOutOfRangeException error) {
+                Console.WriteLine(error.Message);
+            }
+
+			try
+			{
+				FoodProduct foodProduct3 = new FoodProduct("Food", 1350, 39.6, 40, 20, 10);
+                Console.WriteLine(foodProduct3.Name);
+                Console.WriteLine(foodProduct3.Weight);
+                Console.WriteLine(foodProduct3.Temperature);
+                Console.WriteLine(foodProduct3.MaxTemperature);
+                Console.WriteLine(foodProduct3.MinTemperature);
+                Console.WriteLine(foodProduct3.HeatCapacity);
+                Console.WriteLine(foodProduct3.Status);
+                cout << f1.GetChangeTemperature() << endl;
+				cout << f1.GetChangeTemperature() << endl;
+			}
+			catch (string s)
+			{
+				cout << s << endl;
+			}
+			catch (...) {
+				cout << "Что-то пошло не так";
+			}
+
+
+
+
+			//return 0;
+			//}
+
+			}
+	}
+}
